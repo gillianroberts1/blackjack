eed751abffc4ff0241d83f1ae9e70fa995383141
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-01-12 19:49:43 +0000

    comments added

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const Game = require("./Game");
 
+
+// create new instance of the game
+// this will control the flow of the same inclucing initialising the deck, player and dealer
 const blackjackGame = new Game();
 blackjackGame.start();
