daafbc1d83da580023fa0d1e6a886f0cea07786d
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-01-14 14:03:15 +0000

    comments added

--- Deck.js
+++ Deck.js
@@ -1,6 +1,8 @@
 class Deck {
   constructor() {
+    // Define the four suits in a deck of cards
     this.suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+    // Define the ranks and values in a deck of cards
     this.ranks = [
       { rank: "2", value: 2 },
       { rank: "3", value: 3 },
@@ -16,11 +18,15 @@ class Deck {
       { rank: "King", value: 10 },
       { rank: "Ace", value: 11 },
     ];
+
+    // Initialise an empty array to hold the cards in the deck
     this.deck = [];
     this.buildDeck();
   }
 
+  // Build the deck
   buildDeck() {
+    // start with an empty deck
     this.deck = [];
     for (const suit of this.suits) {
       for (const { rank, value } of this.ranks) {
@@ -29,39 +35,29 @@ class Deck {
     }
   }
   size() {
+    // Return the total number of cards currently in the deck
     return this.deck.length;
   }
 
   shuffle() {
-    const totalCards = this.size()
-    for(let currentIndex = totalCards -1; currentIndex > 0; currentIndex--) {
-        const randomIndex = Math.floor(Math.random() * (currentIndex + 1));
-        
-        const tempPosition = this.deck[currentIndex];
-        this.deck[currentIndex] = this.deck[randomIndex];
-        this.deck[randomIndex] = tempPosition;
+    // Shuffle deck using Fisher-Yates shiffle algorithm
+    const totalCards = this.size();
+    for (let currentIndex = totalCards - 1; currentIndex > 0; currentIndex--) {
+      // Pick a random index
+      const randomIndex = Math.floor(Math.random() * (currentIndex + 1));
+      // Swap the current card with the card at the random index
+      const tempPosition = this.deck[currentIndex];
+      this.deck[currentIndex] = this.deck[randomIndex];
+      this.deck[randomIndex] = tempPosition;
     }
   }
 
   draw() {
-    if(this.size() > 0) {
-        return this.deck.pop();
+    // Remove and return the top card from the deck
+    if (this.size() > 0) {
+      return this.deck.pop();
     }
   }
 }
-// initiliase new deck
-// const myDeck = new Deck();
-
-// // shuffle deck
-// myDeck.shuffle()
-
-// const drawnCard = myDeck.draw()
-// console.log('Drawn Card:', drawnCard);
-
-// console.log(myDeck.deck);
-
 
 module.exports = Deck;
-
-
-
