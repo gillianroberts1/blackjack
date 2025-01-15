8963799c09b854154b3161254a85569af4b82f58
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-01-15 18:17:12 +0000

    linting, comment cleanup in hand

--- Hand.js
+++ Hand.js
@@ -3,27 +3,25 @@ class Hand {
     this.cards = [];
   }
 
-  // Add a card to the hand
   addCard(card) {
     this.cards.push(card);
   }
 
-  // get the total value of the hand
   getHandValue() {
     let total = 0;
     let aces = 0;
 
-    // calculate the total value
+    // calculate the total value of all cards in hand
     for (const card of this.cards) {
       total += card.value;
       if (card.rank === "Ace") {
-        aces += 1; //count the number of aces
+        aces += 1; // count the number of aces
       }
     }
 
     // Adjust for aces if the total is over 21
     while (total > 21 && aces > 0) {
-      total -= 10; //count ace as 1 instead of 11
+      total -= 10; // count ace as 1 instead of 11
       aces -= 1;
     }
 
@@ -31,7 +29,6 @@ class Hand {
   }
 
   size() {
-    // Returns the number of cards in the hand
     return this.cards.length;
   }
 }
--- Hand.test.js
+++ Hand.test.js
@@ -1,28 +1,25 @@
-const Hand = require('./Hand');
+const Hand = require("./Hand");
 
-describe('Hand', () => {
-    let hand;
+describe("Hand", () => {
+  let hand;
 
-    beforeEach(() => {
-        hand = new Hand();
-    });
+  beforeEach(() => {
+    hand = new Hand();
+  });
 
-    it('should initialise with no cards', () => {
-        expect(hand.size()).toBe(0);
-      });
+  it("should initialise with no cards", () => {
+    expect(hand.size()).toBe(0);
+  });
 
-    it('should add a card to the hand', () => {
-        hand.addCard({ label: '2', value: 2 });
-        expect(hand.size()).toBe(1);
-    })
+  it("should add a card to the hand", () => {
+    hand.addCard({ label: "2", value: 2 });
+    expect(hand.size()).toBe(1);
+  });
 
-    it('should calculate the total value of the hand', () => {
-        hand.addCard({ label: '2', value: 2 });
-        hand.addCard({ label: 'Jack', value: 10 });
-        expect(hand.size()).toBe(2);
-        expect(hand.getHandValue()).toBe(12);
-    })
-
-
-    
-})
+  it("should calculate the total value of the hand", () => {
+    hand.addCard({ label: "2", value: 2 });
+    hand.addCard({ label: "Jack", value: 10 });
+    expect(hand.size()).toBe(2);
+    expect(hand.getHandValue()).toBe(12);
+  });
+});
